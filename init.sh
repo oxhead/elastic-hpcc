64b9b5e443aa0800b2c97cfb1f5336f084e4b677
Chin-Jung Hsu <chsu6@ncsu.edu>
2015-12-31 13:57:01 -0500

    Fixed a bug when exiting the workspace.

--- init.sh
+++ init.sh
@@ -6,19 +6,19 @@ action_prepare ()
 {
     if [ ! -d "$VENV_DIR" ]; then
         virtualenv $VENV_DIR
-        action_prepare
-        bash --rcfile .venv/bin/activate
-        pip install executor
-        pip install click
-        deactivate
     fi
+    . .venv/bin/activate
+    pip install executor 2>&1 > /dev/null
+    pip install click 2>&1 > /dev/null
+    pip install --editable sbin 2>&1 > /dev/null
+    deactivate
 }
 
 action_init ()
 {
     action_prepare
-    bash --rcfile .venv/bin/activate
-    pip install --editable sbin
+    #bash --rcfile .venv/bin/activate
+    . $VENV_DIR/bin/activate
 }
 
 ### Main
