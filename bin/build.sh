b3645251fa36ad130d49ead624e7a3cdee008d1c
Chin-Jung Hsu <chsu6@ncsu.edu>
2016-01-01 22:33:31 -0500

    Fix a bug: the happens when the build directory does not exist.

--- bin/build.sh
+++ bin/build.sh
@@ -6,5 +6,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # load the env variables
 . $SCRIPT_DIR/env.sh
 
-mkdir -p $BUILD_DIR
 cd $BUILD_DIR && cmake $REPO_DIR && cmake -DCMAKE_BUILD_TYPE=Debug $REPO_DIR && make && make package
--- bin/env.sh
+++ bin/env.sh
@@ -6,8 +6,10 @@ SRC_DIR=`readlink -e $PROJECT_DIR/$REPO_NAME`
 # The separate repo directory for development purpose. Assume in the same level with PROJECT_DIR
 DEV_SRC_DIR=`readlink -e $PROJECT_DIR/../$REPO_NAME`
 REPO_DIR=`if [ -d "$DEV_SRC_DIR" ]; then echo $DEV_SRC_DIR; else echo $SRC_DIR; fi`
-BUILD_DIR=`readlink -e $PROJECT_DIR/build`
 BIN_DIR=`readlink -e $PROJECT_DIR/bin`
+
+mkdir -p "$PROJECT_DIR/build"
+BUILD_DIR=`readlink -e $PROJECT_DIR/build`
 
 echo "src: $SRC_DIR"
 echo "bild: $BUILD_DIR"
