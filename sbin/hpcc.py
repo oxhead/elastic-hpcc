fe576f53f6f95b526d79534cda324d280cd22ab2
Chin-Jung Hsu <chsu6@ncsu.edu>
2016-01-04 01:53:58 -0500

    Add several practical functions.

--- sbin/hpcc.py
+++ sbin/hpcc.py
@@ -6,6 +6,8 @@ import executor
 from executor import execute
 from executor.ssh.client import RemoteCommand
 
+import parallel
+
 @click.group()
 @click.option('--hosts', type=click.Path(exists=True, resolve_path=True))
 @click.option('--host', '-m', multiple=True)
@@ -54,6 +56,25 @@ def uninstall_package(ctx):
         RemoteCommand(host, "dpkg -r hpccsystems-platform", sudo=True, silent=True).start()
 
 @cli.command()
+@click.pass_context
+def clear_log(ctx):
+    with parallel.CommandAgent(show_result=False) as agent:
+        agent.submit_remote_commands(ctx.obj['host_list'], "sudo rm -rf /var/log/HPCCSystems/*", check=False, silent=True)
+
+@cli.command()
+@click.pass_context
+def clear_system(ctx):
+    # @todo: need to make sure the system is turned off.
+    with parallel.CommandAgent(show_result=False) as agent:
+        agent.submit_remote_commands(ctx.obj['host_list'], "sudo rm -rf /var/lib/HPCCSystems/*", check=False, silent=True)
+
+@cli.command()
+@click.pass_context
+def verify_config(ctx):
+    with parallel.CommandAgent(show_result=False) as agent:
+        agent.submit_remote_commands(ctx.obj['host_list'], "md5sum /etc/HPCCSystems/environment.xml", check=False, silent=False)
+
+@cli.command()
 @click.option('-c', '--config', type=click.Path(exists=True, resolve_path=True), default="/etc/HPCCSystems/environment.xml")
 @click.pass_context
 def deploy_config(ctx, config):
@@ -69,25 +90,47 @@ def deploy_config(ctx, config):
 @click.option('-c', '--component', multiple=True, type=click.Choice(['dafilesrv', 'dali', 'dfuserver', 'eclagent', 'eclccserver', 'eclscheduler', 'esp', 'sasha', 'thor', 'roxie']))
 @click.pass_context
 def service(ctx, action, component):
-    for host in reversed(ctx.obj['host_list']):
-        click.echo('{}: service {}'.format(host, action if action is not None else "lookup"))
-        if action == 'list_components':
-            RemoteCommand(host, "sudo service hpcc-init --componentlist", check=False, silent=False).start()
-        elif action == 'list_types':
-            RemoteCommand(host, "sudo service hpcc-init --typelist", check=False, silent=False).start()
-        else: 
-            if len(component) > 0:
-                filtered_components = [n for n in component if n is not "dafilesrv"]
-                if len(filtered_components) > 0:
-                    RemoteCommand(host, "sudo service hpcc-init {} {}".format(" ".join(["-c %s" % n for n in component]), action), check=False, silent=False).start()
+    if action == 'list_components':
+        with parallel.CommandAgent() as agent:
+            cmd = "sudo service hpcc-init --componentlist"
+            agent.submit_remote_commands(ctx.obj['host_list'], cmd, check=False, silent=False)
+    elif action == 'list_types':
+        with parallel.CommandAgent() as agent:
+            cmd = "sudo service hpcc-init --typelist"
+            agent.submit_remote_commands(ctx.obj['host_list'], cmd, check=False, silent=False)
+    else:
+        if len(component) > 0:
+            filtered_components = [n for n in component if n is not "dafilesrv"]
+            if len(filtered_components) > 0:
+                cmd = "sudo service hpcc-init {} {}".format(" ".join(["-c %s" % n for n in component]), action)
+                with parallel.CommandAgent() as agent:
+                    agent.submit_remote_commands(ctx.obj['host_list'], cmd, check=False, silent=False)
                 if 'dafilesrv' in component:
-                    RemoteCommand(host, "sudo service dafilesrv {}".format(action), check=False, silent=False).start()
-            else:
-                RemoteCommand(host, "sudo service hpcc-init {}".format(action), check=False, silent=False).start()
-                RemoteCommand(host, "sudo service dafilesrv {}".format(action), check=False, silent=False).start()
+                    cmd = "sudo service dafilesrv {}".format(action)
+                    with parallel.CommandAgent() as agent:
+                        agent.submit_remote_commands(ctx.obj['host_list'], cmd, check=False, silent=False)
+        else:
+            with parallel.CommandAgent() as agent:
+                agent.submit_remote_commands(ctx.obj['host_list'], "sudo service hpcc-init {}".format(action), check=False, silent=True, capture=True)
+            with parallel.CommandAgent() as agent:
+                agent.submit_remote_commands(ctx.obj['host_list'], "sudo service dafilesrv {}".format(action), check=False, silent=True, capture=True)
 
 @cli.command()
-@click.option('--action', type=click.Choice(['show', 'clear']))
+@click.option('-u', '--username', default='hpcc')
 @click.pass_context
-def log(ctx, action):
-    pass
+def deploy_key(ctx, username):
+    '''This command deploy the current user's key to a remote user.
+
+    The current implmentation might be insecure.
+    '''
+    with parallel.CommandAgent(show_result=False) as agent:
+        for host in ctx.obj['host_list']:
+            agent.submit_command("scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ~/.ssh/id_rsa* {}@{}:/tmp".format(username, host), check=True, silent=True)
+    with parallel.CommandAgent(show_result=False) as agent:
+        agent.submit_remote_commands(ctx.obj['host_list'], "sudo cp /tmp/id_rsa* /home/{}/.ssh; sudo rm -rf /tmp/id_rsa*".format(username), check=True, silent=True)
+    with parallel.CommandAgent(show_result=False) as agent:
+        agent.submit_remote_commands(ctx.obj['host_list'], "sudo cat /home/{}/.ssh/id_rsa.pub >> /home/{}/.ssh/authorized_keys".format(username, username), check=True, silent=True)
+    with parallel.CommandAgent(show_result=False) as agent:
+        agent.submit_remote_commands(ctx.obj['host_list'], "sudo chmod 644 /home/{}/.ssh/*".format(username), check=True, silent=True)
+    with parallel.CommandAgent(show_result=False) as agent:
+        agent.submit_remote_commands(ctx.obj['host_list'], "sudo chown {} /home/{}/.ssh/*".format(username, username), check=True, silent=True)
--- sbin/vcl.py
+++ sbin/vcl.py
@@ -6,6 +6,8 @@ import executor
 from executor import execute
 from executor.ssh.client import RemoteCommand
 
+import parallel
+
 @click.group()
 @click.option('--hosts', type=click.Path(exists=True, resolve_path=True))
 @click.option('--host', '-m', multiple=True)
@@ -32,6 +34,27 @@ def add_key(ctx, username):
         execute("scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ~/.ssh/id_rsa* {}@{}:~/.ssh/".format(username, host), silent=True)
 
 @cli.command()
+@click.argument('user')
+@click.option('--from_user', default=lambda: os.getenv('USER'))
+@click.pass_context
+def deploy_key(ctx, user, from_user):
+    '''This command deploy the current user's key to a remote user.
+
+    The current implmentation might be insecure and only works for rsa key.  This also assumes the home directory is located at /home.
+    '''
+    with parallel.CommandAgent(show_result=False) as agent:
+        for host in ctx.obj['host_list']:
+            agent.submit_command("scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /home/{}/.ssh/id_rsa* {}@{}:/tmp".format(from_user, from_user, host), check=True, silent=True)
+    with parallel.CommandAgent(show_result=False) as agent:
+        agent.submit_remote_commands(ctx.obj['host_list'], "sudo cp /tmp/id_rsa* /home/{}/.ssh; sudo rm -rf /tmp/id_rsa*".format(user), check=True, silent=True)
+    with parallel.CommandAgent(show_result=False) as agent:
+        agent.submit_remote_commands(ctx.obj['host_list'], "sudo bash -c 'cat /home/{}/.ssh/id_rsa.pub >> /home/{}/.ssh/authorized_keys'".format(user, user), check=True, silent=True)
+    with parallel.CommandAgent(show_result=False) as agent:
+        agent.submit_remote_commands(ctx.obj['host_list'], "sudo chmod 600 /home/{}/.ssh/id_rsa*; sudo chmod 644 /home/{}/.ssh/authorized_keys".format(user, user), check=True, silent=True)
+    with parallel.CommandAgent(show_result=False) as agent:
+        agent.submit_remote_commands(ctx.obj['host_list'], "sudo chown {} /home/{}/.ssh/id_rsa*; sudo chown {} /home/{}/.ssh/authorized_keys".format(user, user, user, user), check=True, silent=True)
+
+@cli.command()
 @click.option('--user', '-m', multiple=True)
 @click.pass_context
 def fix_ssh(ctx, user):
@@ -43,8 +66,9 @@ def fix_ssh(ctx, user):
     user_list = user if (user is not None) and (len(user) > 0)  else [getpass.getuser(), "hpcc"]
     for host in ctx.obj['host_list']:
         sshd_config = "/etc/ssh/sshd_config"
-        RemoteCommand(host, "cp {} {}.bak".format(sshd_config, sshd_config), ignore_known_hosts=True, sudo=True).start()
-        RemoteCommand(host, 'sed -i "s/^AllowUsers.*/AllowUsers root {}/" {}'.format(" ".join(user_list), sshd_config), ignore_known_hosts=True, sudo=True).start()
+        RemoteCommand(host, "sudo cp {} {}.bak".format(sshd_config, sshd_config), ignore_known_hosts=True).start()
+        RemoteCommand(host, 'sudo sed -i "s/^AllowUsers.*/AllowUsers root {}/" {}'.format(" ".join(user_list), sshd_config), ignore_known_hosts=True).start()
+        RemoteCommand(host, 'sudo service ssh reload', ignore_known_hosts=True).start()
 
 @cli.command()
 @click.pass_context
