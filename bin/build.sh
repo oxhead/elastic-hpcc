684ab833871a0ebfa147a57c9dbe08bf89cd54fa
Chin-Jung Hsu <chsu6@ncsu.edu>
2016-01-04 01:55:40 -0500

    Remove unnecessary step in the building script.

--- bin/build.sh
+++ bin/build.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # load the env variables
 . $SCRIPT_DIR/env.sh
 
-cd $BUILD_DIR && cmake $REPO_DIR && cmake -DCMAKE_BUILD_TYPE=Debug $REPO_DIR && make && make package
+cd $BUILD_DIR && cmake -DCMAKE_BUILD_TYPE=Debug $REPO_DIR && make && make package
