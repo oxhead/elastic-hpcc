56657d674bf5fd1b30ac5be877edc62fde4d10b4
Chin-Jung Hsu <chsu6@ncsu.edu>
2016-05-02 23:27:30 -0400

    Script for new benchmark.

--- mybenchmark/simple_workload/test_workload.py
+++ mybenchmark/simple_workload/test_workload.py
@@ -13,13 +13,14 @@ from elastic.util import helper
 
 def main():
     init.setup_logging(config_path="conf/logging.yaml", log_dir="logs", component="benchmark")
-    hpcc_cluster = HPCCCluster.parse_config("/etc/HPCCSystems/source/hpcc_t5_r5_cyclic.xml")
-    hpcc_cluster = HPCCCluster.parse_config("/etc/HPCCSystems/source/hpcc_16r_cyclic_2replica.xml")
+    # hpcc_cluster = HPCCCluster.parse_config("/etc/HPCCSystems/source/hpcc_t5_r5_cyclic.xml")
+    #hpcc_cluster = HPCCCluster.parse_config("/etc/HPCCSystems/source/hpcc_16r_cyclic_2replica.xml")
+    hpcc_cluster = HPCCCluster.parse_config("/etc/HPCCSystems/source/elastic_cyclic_4node_2replica.xml")
     #benchmark_config = BenchmarkConfig.parse_file("/home/chsu6/elastic-hpcc/conf/6driver.yaml")
     benchmark_config = BenchmarkConfig.parse_file("/home/chsu6/elastic-hpcc/conf/1driver.yaml")
 
     script_dir = os.path.dirname(os.path.realpath(__file__))
-    workload_config = os.path.join(script_dir, "workload_simple2.yaml")
+    workload_config = os.path.join(script_dir, "workload_new_bm2.yaml")
     workload = Workload.parse_config(workload_config)
     workload_timeline = WorkloadExecutionTimeline.from_workload(workload)
 
